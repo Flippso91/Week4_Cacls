67b78221cecd046ce2ab0d58857ff3bc9b8d4c14
theonlydevin <g004074545@atu.ie>
2022-10-10 11:06:39 +0100

    Commitin add feature

--- src/test/java/ie/atu/Calc.java
+++ src/test/java/ie/atu/Calc.java
@@ -1,7 +1,38 @@
 package ie.atu;
 
-public class Calc {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
+public class Calc {
+
+        public static void main(String[] args) {
+            add();
+
+        }
+
+        public static void add()
+        {
+            System.out.println("Calculator startup");
+            Scanner input = new Scanner(System.in);
+
+            System.out.println("Please enter a number ");
+            Scanner inputs = new Scanner(System.in);
+            int firstNumber = input.nextInt();
+
+            System.out.println("Please enter a second number ");
+            int secondNumber = input.nextInt();
+
+            int totalvalue =firstNumber + secondNumber;
+            System.out.println("Total amounts to  : " +totalvalue);
+
+
+        }
     }
+
+
+
+
+
+
+
+
 }
