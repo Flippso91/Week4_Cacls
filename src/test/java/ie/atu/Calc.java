511a0cfb7ddc1f87d8c662f1fabb0f7528269895
ellenmcintyre123 <g00389482@atu.ie>
2022-10-10 11:14:31 +0100

    feature to subtract added

--- src/test/java/ie/atu/Calc.java
+++ src/test/java/ie/atu/Calc.java
@@ -2,6 +2,20 @@ package ie.atu;
 
 public class Calc {
     public static void main(String[] args) {
+    add delete();
+    }
+    //this method takes in a values and subtraced them
+    public static void delete() {
+        System.out.println("The numbers you enter will be subtracted");
+        System.out.println("Please enter a number");
+        Scanner input = new Scanner(System.in);
+        int firstNumber = input.nextInt();
+        System.out.println("You entered: " + firstNumber);
+        System.out.println("please enter another number");
+        int secondNumber = input.nextInt();
+        System.out.println("You entered: " + secondNumber);
+        int total = firstNumber - secondNumber;
+        System.out.println("when subtraced the answer is:" + total);
 
     }
 }
